a74818b9b476105cd51c29456660bcb973f4575d
krishna vishwakama <146770906+krishnaprince7@users.noreply.github.com>
2025-08-24 09:07:58 +0530

    Update Technology.jsx

--- src/componenets/Technology.jsx
+++ src/componenets/Technology.jsx
@@ -49,11 +49,21 @@ const Technology = () => {
       <h3 className="mt-5 text-xl sm:text-2xl font-semibold">{card.title}</h3>
 
       <div className="mt-5 text-sm sm:text-[15px] text-gray-600 leading-7">
-        <div className="flex flex-wrap gap-x-3 gap-y-2">
-          <span>.NET</span> <span>C/C++</span> <span>DJANGO</span> <span>Firebase</span> <span>GOLANG</span> 
-          <span>SYMFONY</span> <span>LARAVEL</span> <span>NODE</span> <span>PHP</span> <span>PYTHON</span>
-          <span>RUBY ON RAILS</span> <span>JAVA</span>
-        </div>
+       <div className="flex flex-wrap gap-3">
+        <span>.NET</span>
+        <span>C/C++</span>
+        <span>DJANGO</span>
+        <span>Firebase</span>
+        <span>GOLANG</span>
+        <span>SYMFONY</span>
+        <span>LARAVEL</span>
+        <span>NODE</span>
+        <span>PHP</span>
+        <span>PYTHON</span>
+        <span>RUBY ON RAILS</span>
+        <span>JAVA</span>
+      </div>
+
       </div>
     </div>
   ))}
